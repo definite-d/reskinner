cd7cea4a3440352d71b55b9d4124ee1955f4314a
Divine U. Afam-Ifediogor <38317208+definite-d@users.noreply.github.com>
2025-10-12 03:19:24 +0100

    feat: add default theme name, the rest of the elements, and use `sg` properly.

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,7 @@ from .sg import sg
 
 DEFAULT_ANIMATED_RESKIN_DURATION = 450
 DISABLED_COLOR = "#A3A3A3"
+DEFAULT_THEME_NAME = "GrayGrayGray"
 
 class InterpolationMode(StrEnum):
     HSL = 'hsv'
@@ -12,21 +13,36 @@ class InterpolationMode(StrEnum):
 
 class SGElement(StrEnum):
     BUTTON = "button",
-    HORIZONTAL_SEPARATOR = "horizontalseparator",
+    BUTTONMENU = "buttonmenu"
+    CANVAS = "canvas"
+    CHECKBOX = "checkbox"
+    COLUMN = "column"
+    COMBO = "combo"
+    FRAME = "frame"
+    GRAPH = "graph"
+    HORIZONTALSEPARATOR = "horizontalseparator",
+    IMAGE = "image"
+    INPUT = "input"
     LISTBOX = "listbox",
+    MENU = "menu"
     MULTILINE = "multiline",
+    OPTIONMENU = "optionmenu"
+    PANE = "pane"
     PROGRESSBAR = "progressbar",
+    RADIO = "radio"
     SIZEGRIP = "sizegrip",
+    SLIDER = "slider"
     SPIN = "spin",
+    STATUSBAR = "statusbar"
     TABGROUP = "tabgroup",
     TABLE = "table",
     TEXT = "text",
     TREE = "tree",
-    VERTICAL_SEPARATOR = "verticalseparator",
+    VERTICALSEPARATOR = "verticalseparator",
 
 NON_GENERIC_ELEMENTS = [
     SGElement.BUTTON,
-    SGElement.HORIZONTAL_SEPARATOR,
+    SGElement.HORIZONTALSEPARATOR,
     SGElement.LISTBOX,
     SGElement.MULTILINE,
     SGElement.PROGRESSBAR,
@@ -36,7 +52,7 @@ NON_GENERIC_ELEMENTS = [
     SGElement.TABLE,
     SGElement.TEXT,
     SGElement.TREE,
-    SGElement.VERTICAL_SEPARATOR,
+    SGElement.VERTICALSEPARATOR,
 ]
 
 _COLOR_MAPPING = {
@@ -50,7 +66,7 @@ _COLOR_MAPPING = {
 }
 
 class ScrollbarColorKey(StrEnum):
-    TROUGH = _COLOR_MAPPING[ttk_part_mapping_dict["Trough Color"]]
-    FRAME = _COLOR_MAPPING[ttk_part_mapping_dict["Frame Color"]]
-    BACKGROUND = _COLOR_MAPPING[ttk_part_mapping_dict["Background Color"]]
-    ARROW = _COLOR_MAPPING[ttk_part_mapping_dict["Arrow Button Arrow Color"]]
+    TROUGH = _COLOR_MAPPING[sg.ttk_part_mapping_dict["Trough Color"]]
+    FRAME = _COLOR_MAPPING[sg.ttk_part_mapping_dict["Frame Color"]]
+    BACKGROUND = _COLOR_MAPPING[sg.ttk_part_mapping_dict["Background Color"]]
+    ARROW = _COLOR_MAPPING[sg.ttk_part_mapping_dict["Arrow Button Arrow Color"]]
