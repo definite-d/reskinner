a551cd378c9da09610a140531e752a4aadf8051d
Divine U. Afam-Ifediogor <38317208+definite-d@users.noreply.github.com>
2025-10-12 03:18:15 +0100

    fix: exception handling and better file organization

--- src/sg.py
+++ src/sg.py
@@ -1,10 +1,16 @@
+prompt = """
+Neither PySimpleGUI (https://github.com/PySimpleGUI/PySimpleGUI/) nor FreeSimpleGUI (https://github.com/spyoungtech/FreeSimpleGUI) are installed.
+
+You should install one or the other with the `psg` and `fsg` optional dependency groups respectively, e.g.:
+`pip install reskinner[psg]` for PySimpleGUI support.
+"""
+
 try:
     import PySimpleGUI as sg
 except ImportError:
-    import FreeSimpleGUI as sg
-except ImportError:
-    prompt = """Neither PySimpleGUI (https://github.com/PySimpleGUI/PySimpleGUI/) nor FreeSimpleGUI (https://github.com/spyoungtech/FreeSimpleGUI) are installed.
-You should install one or the other with the `psg` and `fsg` optional dependency groups respectively, e.g.:
-`pip install reskinner[psg]` for PySimpleGUI support.
-    """
-    raise EnvironmentError(prompt)
+    try:
+        import FreeSimpleGUI as sg
+    except ImportError:
+        raise EnvironmentError(prompt)
+
+__all__ = ["sg"]
