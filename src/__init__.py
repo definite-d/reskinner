68827ce662830d24b19c7f28610faa9665dcd550
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>
2025-10-13 08:41:25 +0100

    feat: add `__version__` module for programmatically obtaining the version of the package from its metadata
    Signed-off-by: Divine U. Afam-Ifediogor <38317208+definite-d@users.noreply.github.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .reskinner import reskin, toggle_transparency
+from .__version__ import __version__
 
-__all__ = ["reskin", "toggle_transparency"]
+__all__ = ["reskin", "toggle_transparency", "__version__"]
