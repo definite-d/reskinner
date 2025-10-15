3577e2397fff84ac43c6d6981e97b3c95128c52d
Divine U. Afam-Ifediogor <38317208+definite-d@users.noreply.github.com>
2025-10-15 08:58:59 +0100

    feat: include `sg` and `SG_LIB` into the `__init__.py`
    Signed-off-by: Divine U. Afam-Ifediogor <38317208+definite-d@users.noreply.github.com>

--- src/reskinner/__init__.py
+++ src/reskinner/__init__.py
@@ -1,4 +1,5 @@
 from .__version__ import __version__
 from .reskinner import reskin, toggle_transparency
+from .sg import sg, SG_LIB
 
-__all__ = ["reskin", "toggle_transparency", "__version__"]
+__all__ = ["reskin", "toggle_transparency", "__version__", "sg", "SG_LIB"]
