53225fbf82d61c2bf0e32f3be94892a52f75df50
Divine U. Afam-Ifediogor <38317208+definite-d@users.noreply.github.com>
2025-10-15 08:57:44 +0100

    feat: move reskinner into its own sub-module
    Signed-off-by: Divine U. Afam-Ifediogor <38317208+definite-d@users.noreply.github.com>

--- src/reskinner/sg.py
+++ src/reskinner/sg.py
@@ -4,13 +4,14 @@ Neither PySimpleGUI (https://github.com/PySimpleGUI/PySimpleGUI/) nor FreeSimple
 You should install one or the other with the `psg` and `fsg` optional dependency groups respectively, e.g.:
 `pip install reskinner[psg]` for PySimpleGUI support.
 """
-
+SG_LIB = "psg"
 try:
     import PySimpleGUI as sg
 except ImportError:
     try:
         import FreeSimpleGUI as sg
+        SG_LIB = "fsg"
     except ImportError:
         raise EnvironmentError(prompt)
 
-__all__ = ["sg"]
+__all__ = ["sg", "SG_LIB"]
